2d2c8ce38704f2af336dff09a3b65e3d57a0d6e7
kasunkv <kvkrusl@gmail.com>
2016-10-21 14:07:21 -0700

    Added basic tests to ensure environment is working. #4

--- sample.spec.ts
+++ sample.spec.ts
@@ -1,9 +1,21 @@
 import {browser, element, by, By} from 'protractor';
 
 describe('Protractor-TypeScript Starter', () => {
-    it('Should navigate to the demo website', () => {
+    beforeEach(() => {
         browser.get('http://amasik.com/demo/angularjs/angular-app/');
+    });
 
+    it('Should navigate to the demo website',() => {
+        expect(browser.getTitle()).toEqual('AngularJS POC');
+    });
+
+    it('Should Login to the dashboard page', () => {        
         element(by.buttonText('Sign In')).click();
+        expect(browser.getLocationAbsUrl()).toContain('dashboard');
+    });
+
+    it('Should logout from the application', () => {
+        element.all(by.className('dropdown-toggle')).get(1).click();
+        element(by.linkText('Logout')).click();
     });
 });
