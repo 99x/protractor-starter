43b68103ddc5c58b7c8adfd765134636402a7d72
kasunk@99x.lk <kasunk@99x.lk>
2017-08-23 08:20:24 +0530

    Fixed the issues in the waitHelper.ts file due to types not being found

--- helpers/waitHelper.ts
+++ helpers/waitHelper.ts
@@ -1,5 +1,5 @@
 /*global module, element, protractor, by, browser, expect*/
-import { browser, ExpectedConditions, element, protractor, ElementFinder, Ptor, ProtractorExpectedConditions } from 'protractor';
+import { browser, ExpectedConditions, element, protractor, ElementFinder, Ptor, ProtractorExpectedConditions, promise as WebDriverPromise } from 'protractor';
 import { Promise } from 'es6-promise';
 
 export class WaitHelper {
@@ -9,55 +9,56 @@ export class WaitHelper {
         WaitHelper.EC = protractor.ExpectedConditions;
     }
 
-    static waitUntilAlertIsPresent(): webdriver.promise.Promise<any> {
+
+    static waitUntilAlertIsPresent(): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.alertIsPresent(), 5000);
     }
 
-    static waitElementToClickable(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitElementToClickable(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.elementToBeClickable(element), 5000);
     }
 
-    static waitUntilTextToBePresentInElement(element: ElementFinder, str: string): webdriver.promise.Promise<any> {
+    static waitUntilTextToBePresentInElement(element: ElementFinder, str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.textToBePresentInElement(element, str), 5000);
     }
 
-    static waitUntilTextToBePresentInElementValue(element: ElementFinder, str: string): webdriver.promise.Promise<any> {
+    static waitUntilTextToBePresentInElementValue(element: ElementFinder, str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.textToBePresentInElementValue(element, str), 5000);
     }
 
-    static waitUntilTitleToContains(str: string): webdriver.promise.Promise<any> {
+    static waitUntilTitleToContains(str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.titleContains(str), 5000);
     }
 
-    static waitUntilTitleIs(str: string): webdriver.promise.Promise<any> {
+    static waitUntilTitleIs(str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.titleIs(str), 5000);
     }
 
-    static waitUntilUrlContains(str: string): webdriver.promise.Promise<any> {
+    static waitUntilUrlContains(str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.urlContains(str), 5000);
     }
 
-    static waitUrlIsEqualsTo(str: string): webdriver.promise.Promise<any> {
+    static waitUrlIsEqualsTo(str: string): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.urlIs(str), 5000);
     }
 
-    static waitUntilPresenceOfElement(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitUntilPresenceOfElement(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.presenceOf(element), 5000);
     }
 
-    static waitUntilStalenessOfElement(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitUntilStalenessOfElement(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.stalenessOf(element), 5000);
     }
 
-    static waitUntilElementToVisible(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitUntilElementToVisible(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.visibilityOf(element), 5000);
     }
 
-    static waitUntilElementToInvisible(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitUntilElementToInvisible(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.invisibilityOf(element), 5000);
     }
 
-    static waitUntilElementToBeSelected(element: ElementFinder): webdriver.promise.Promise<any> {
+    static waitUntilElementToBeSelected(element: ElementFinder): WebDriverPromise.Promise<any> {
         return browser.wait(this.EC.elementToBeSelected(element), 5000);
     }
 }
